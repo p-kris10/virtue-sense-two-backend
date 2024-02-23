ddf49a8291dbd14358dfdeea5e76b98b3fd8d935
Kristen Pereira <26pkristen@gmail.com>
2024-02-23 00:47:18 -0500

    added logging

--- app.py
+++ app.py
@@ -7,25 +7,50 @@ import tempfile
 import os
 from scipy.io import wavfile
 import string
+import json
 from flask_cors import CORS
+import random
 # from flask_ngrok import run_with_ngrok
-
-
 
 model = whisper.load_model("tiny")
 
-
-
 app = Flask(__name__)
 CORS(app)
+
+# Read questions from the JSON file
+with open('questions.json') as f:
+    questions_json = json.load(f)
+
+
+def log(question,resp):
+    with open('log.txt', 'a') as f:
+        
+        f.write(f"{question} : {resp}\n")
+
+
 @app.route('/transcribe_audio', methods=['POST'])
 def transcribe_audio():
     if 'audio_data' not in request.files:
         return jsonify({'error': 'No audio file part'})
 
     audio_file = request.files['audio_data'].read()
-    #write code to appropriately transcribe the audio file
+    count = request.form['count']
+    question = request.form['question']
+    try:
+        count = int(count)
+    except ValueError:
+        return jsonify({'error': 'Invalid count'})
+
     
+    # print(count)
+    # print(type(count))
+
+    if count > 3:
+        #log as invalid
+        log(question,"Invalid response")
+
+        
+
     
     if audio_file:
         with open("temp.wav", "wb") as fh:
@@ -33,6 +58,8 @@ def transcribe_audio():
 
     result = model.transcribe("temp.wav")
     res = result["text"]
+    # Delete the temporary file
+    os.remove("temp.wav")
     # Split into words
     words = res.split()
 
@@ -50,13 +77,21 @@ def transcribe_audio():
     else:
         res = res[0]
         if res.lower() == "no":
+            log(question,"No")
             return jsonify({'message': "no"})
         elif res.lower() == "yes":
+            log(question,"Yes")
             return jsonify({'message': "yes"})
         else:
             return jsonify({'message': res})
 
     return jsonify({'message': result["text"]})
+
+@app.route('/get_question', methods=['GET'])
+def get_random_question():
+    # Select a random question from the JSON
+    random_question = random.choice(questions_json)
+    return jsonify(random_question)
 
 @app.route('/', methods=['GET'])
 def index():
